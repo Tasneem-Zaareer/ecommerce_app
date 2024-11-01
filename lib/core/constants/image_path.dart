b2086b9ed3e3a57bd54ca58734c3f6ecdaf17f9d
Tasneem-Zaareer <tasneem.zareer@gmail.com>
2024-11-01 19:27:58 +0400

    add image path in AppImage class

--- lib/core/constants/image_path.dart
+++ lib/core/constants/image_path.dart
@@ -3,8 +3,11 @@ class AppImages {
   static const String onBoardingImage2 = 'assets/images/png/gift2.png';
   static const String onBoardingImage3 = 'assets/images/png/gift3.png';
   static const String imageNotFound = 'assets/images/png/gift2.png';
-  static const String dadTest = 'assets/images/jpg/dad1.jpg';
-  static const String dad2 = 'assets/images/jpg/dad2.jpg';
-  static const String dad3 = 'assets/images/jpg/dad3.jpg';
-  static const String dad4 = 'assets/images/jpg/dad4.jpg';
+  //----
+  static const String homeHeaderImage = 'assets/images/home_header.jpg';
+    static const String categoryTestImage = 'assets/images/category_test.jpg';
+        static const String categoryTestImage2 = 'assets/images/category_test2.jpg';
+
+
+
 }
